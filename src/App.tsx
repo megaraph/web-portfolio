16cb7a6d71fcbc9c4acf4137504cc5ce7d0822f5
megaraph <47447126+megaraph@users.noreply.github.com>
2025-08-24 03:05:51 +0800

    feat: video showcase

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import Hero from "./sections/Hero";
 import About from "./sections/About";
 import Projects from "./sections/Projects";
 import Contact from "./sections/Contact";
+import VideoShowcase from "./sections/VideoShowcase";
 
 function App() {
     return (
         <div className="min-h-screen">
             <Hero />
+            <VideoShowcase />
             <About />
             <Projects />
             <Contact />
